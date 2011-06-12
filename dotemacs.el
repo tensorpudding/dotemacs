d19ae191481e3c6c13a21ae35f03133864f45cb5
Michael Moorman <tensorpuddin@devio.us>
2011-06-12 17:42:54 -0400

    fixed some things

--- dotemacs.el
+++ dotemacs.el
@@ -1,4 +1,3 @@
--*- Mode: Emacs-Lisp -*-
 (add-to-list 'load-path "~/.emacs.d")
 (load-theme 'solarized-dark)
 (enable-theme 'solarized-dark)
@@ -25,15 +24,15 @@
 
 (add-hook 'prog-mode-hook 'turn-on-line-numbering)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("0174d99a8f1fdc506fa54403317072982656f127" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("0174d99a8f1fdc506fa54403317072982656f127" default))))
