073d27046a9aaf3becdea77fcc7bf78a9b994a15
Michael Moorman <tensorpuddin@devio.us>
2011-06-13 19:49:51 -0400

    more customizations, disabled fci, removed manual theme code

--- dotemacs.el
+++ dotemacs.el
@@ -1,6 +1,4 @@
 (add-to-list 'load-path "~/.emacs.d")
-(load-theme 'solarized-dark)
-(enable-theme 'solarized-dark)
 
 (require 'package)
 (add-to-list 'package-archives
@@ -23,6 +21,7 @@
   (setq linum-format "%5d "))
 
 (add-hook 'prog-mode-hook 'turn-on-line-numbering)
+;;(add-hook 'prog-mode-hook 'fci-mode)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -35,4 +34,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("0174d99a8f1fdc506fa54403317072982656f127" default))))
+ '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(background-color "#042028")
+ '(background-mode dark)
+ '(cursor-color "#708183")
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes (quote ("5f5644eaf825f7ef4a7f8137540821a3a2ca009e" "e74544a27828e83ae2fc825cd77b7a2c6490a733" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "a22ec513302bbedd659ec3571ca9911b12f7ebe7" "f8f89bc6e899f305cb9766d15bb02205eb462528" "ca7fdd2df444433d6f6097a5b5c24d8949c546a3" "0174d99a8f1fdc506fa54403317072982656f127" default)))
+ '(foreground-color "#708183"))
