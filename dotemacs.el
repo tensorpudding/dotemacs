591ae221b6b6053cd3db6b270be5b87893d0e2fe
Michael Moorman <tensorpuddin@devio.us>
2011-09-25 19:00:49 -0400

    amending c++ indent style

--- dotemacs.el
+++ dotemacs.el
@@ -37,6 +37,7 @@
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(background-color "#042028")
  '(background-mode dark)
+ '(c-default-style (quote ((c++-mode . "stroustrup") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
  '(cursor-color "#708183")
  '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes (quote ("5f5644eaf825f7ef4a7f8137540821a3a2ca009e" "e74544a27828e83ae2fc825cd77b7a2c6490a733" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "a22ec513302bbedd659ec3571ca9911b12f7ebe7" "f8f89bc6e899f305cb9766d15bb02205eb462528" "ca7fdd2df444433d6f6097a5b5c24d8949c546a3" "0174d99a8f1fdc506fa54403317072982656f127" default)))
