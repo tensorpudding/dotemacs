90fe41733e32b91560544ebf1654641d237e8939
Michael Moorman <tensorpuddin@devio.us>
2011-09-28 03:16:36 -0400

    changed name to init.el

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
   (interactive)
   (if window-system
       (progn
-	(add-to-list 'default-frame-alist (cons 'width 90))
+	(add-to-list 'default-frame-alist (cons 'width 85))
 	(add-to-list 'default-frame-alist 
 		     (cons 'height (/ (- (x-display-pixel-height) 60)
 				      (frame-char-height)))))))
@@ -38,7 +38,9 @@
  '(background-color "#042028")
  '(background-mode dark)
  '(c-default-style (quote ((c++-mode . "stroustrup") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ '(column-number-mode t)
  '(cursor-color "#708183")
- '(custom-enabled-themes (quote (zenburn)))
+ '(custom-enabled-themes (quote (adwaita)))
  '(custom-safe-themes (quote ("5f5644eaf825f7ef4a7f8137540821a3a2ca009e" "e74544a27828e83ae2fc825cd77b7a2c6490a733" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "a22ec513302bbedd659ec3571ca9911b12f7ebe7" "f8f89bc6e899f305cb9766d15bb02205eb462528" "ca7fdd2df444433d6f6097a5b5c24d8949c546a3" "0174d99a8f1fdc506fa54403317072982656f127" default)))
+ '(fill-column 70)
  '(foreground-color "#708183"))
