484224cd42732c0ba9bff08c1409f498475f064f
Michael Moorman <tensorpuddin@devio.us>
2012-02-27 11:44:43 -0500

    Updating to use sendmail for email

--- init.el
+++ init.el
@@ -72,4 +72,5 @@
  '(custom-safe-themes (quote ("5f5644eaf825f7ef4a7f8137540821a3a2ca009e" "e74544a27828e83ae2fc825cd77b7a2c6490a733" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "a22ec513302bbedd659ec3571ca9911b12f7ebe7" "f8f89bc6e899f305cb9766d15bb02205eb462528" "ca7fdd2df444433d6f6097a5b5c24d8949c546a3" "0174d99a8f1fdc506fa54403317072982656f127" default)))
  '(fill-column 70)
  '(foreground-color "#708183")
- '(scheme-program-name "racket"))
+ '(scheme-program-name "racket")
+ '(send-mail-function (quote sendmail-send-it)))
